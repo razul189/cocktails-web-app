52c6c9ce1dc67f2b61f49f4b53464f03089a3ced
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-04-24 11:43:48 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
     getCocktails()
 })
 
-function getCocktails (){
-    const ul = .getElementById("show-list")
-    fetch ("URL")
-        .then (response => response.json())
-        .then (data => {
-            console.log(data)
+function getCocktails() {
+    const ul = document.getElementById("show-list");
+    fetch("URL")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
         })
 }
+
+// fetch(URL)
+//     .then(response => {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => console.log(error));
