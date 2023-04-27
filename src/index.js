8481bba0ce4ab61cace07add1a21c80bcfb39da5
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-04-27 15:43:25 -0400

    Eventlistener created for link click

--- src/index.js
+++ src/index.js
@@ -2,18 +2,32 @@ const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=cocktail"
 
 document.addEventListener("DOMContentLoaded", () => {
     getCocktails()
+    document.getElementById('cocktails').addEventListener('click', getCocktails)
 })
 
 function getCocktails(){
     const ul = document.getElementById("drink-list")
+    const info = document.getElementById('info')
+    info.innerHTML = ""
+    ul.innerHTML = ""
     fetch(URL)
         .then(response => response.json())
         .then(data => {
             data.drinks.forEach(drink => {
-                ul.innerHTML += `<li>${drink.strDrink}</li>`
+                ul.innerHTML += `<li><a href="#" data-id="${drink.idDrink}">${drink.strDrink}</a></li>`
             })
+            clickableLink()
         })
         .catch(error => console.log(error))
 }
 
-  
+const clickableLink = () => {
+    const cocktails= document.querySelectorAll("a")
+    cocktails.forEach((drink) => {
+        drink.addEventListener("click", displayDrink)
+    })
+}
+
+
+
+
